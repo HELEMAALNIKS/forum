a79cfc3db601290a7669e94686ce788c06649c09
HELEMAAL_NIKS <jaron@hmcnetwork.nl>
2019-10-07 13:53:47 +0200

    Migrations database fields

--- database/migrations/2019_09_23_135142_create_users_table.php
+++ database/migrations/2019_09_23_135142_create_users_table.php
@@ -15,8 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('firstname');
-            $table->string('lastname');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
